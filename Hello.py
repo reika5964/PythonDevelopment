48b26769e5aad60375c61eaa1c4dd9aaf55fe935
Napatsara <nigawa75@gmail.com>
2022-08-08 16:47:42 +0900

    database link

--- Hello.py
+++ Hello.py
@@ -1,11 +1,16 @@
 from flask import Flask,render_template
+import pymysql
 
 app = Flask(__name__)
+conn=pymysql.connect(host='localhost',user='root',password='',database='studentdb')
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    with conn:
+        cur=conn.cursor()
+        cur.execute("select * from student")
+        rows=cur.fetchall()
+        return render_template('index.html',datas=rows)
  
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
